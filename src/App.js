f82c0932c764904518e8c69e463d7f3a24f37b87
zubair-ashraf <zubair8767844@gmail.com>
2020-10-08 16:16:44 +0500

    Auth provider configuration

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import ApolloProvider from "./ApolloProvider";
+import { AuthProvider } from "./context/auth";
 import "./App.scss";
 
 function App() {
   return (
     <ApolloProvider>
-      <BrowserRouter>
-        <Container>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-          </Switch>
-        </Container>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Container>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
+            </Switch>
+          </Container>
+        </BrowserRouter>
+      </AuthProvider>
     </ApolloProvider>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Row, Col, Form, Button } from "react-bootstrap";
 import { gql, useLazyQuery } from "@apollo/client";
 import { Link } from "react-router-dom";
+import { useAuthDispatch } from "../context/auth";
 
 const LOGIN_USER = gql`
   query login($username: String!, $password: String!) {
@@ -20,9 +21,12 @@ export default function Login(props) {
     password: "",
   });
   const [errors, setErrors] = useState({});
+
+  const dispatch = useAuthDispatch();
+
   const [loginUser, { loading }] = useLazyQuery(LOGIN_USER, {
     onCompleted: (data) => {
-      localStorage.setItem("token", data.login.token);
+      dispatch({ type: "LOGIN", payload: data.login });
       props.history.push("/");
     },
     onError: (err) => setErrors(err.graphQLErrors[0].extensions.errors),
